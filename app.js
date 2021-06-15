06e350f0aac494160f5145f0bdc7316d3983d07b
David <vasquezd@oregonstate.edu>
2021-06-15 16:56:18 -0700

    express app

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
-const express = require('express')
-const port = process.env.port || 3003;
-const app = express()
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
 
-app.listen(port, () => {
-  console.log("Server is up and listening on 3003...")
-})
+var log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
 
-app.get("/", (req, res) => {
-    console.log("hiya!");
-    res.send("hiya!");
-    res.end()
-})
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
 
-//ROUTE 1: Simple GET Request 
-app.get('/user/:username', (req, res) => {
-    console.log("Fetching user with id: " + req.params.username);  
-    res.send("User " + req.params.username);
-    res.end();
-})
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
 
-//ROUTE 2: Simple Users Response
-app.get("/users", (req, res) => {
-    const user1 = {firstName: "David", lastName: "V"}
-    const user2 = {firstName: "Frodo", lastName: "B"}
-    const user3 = {firstName: "Bilbo", lastName: "B"}
-    res.json([user1, user2, user3])
-})
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
 
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
+    }
+});
 
- 
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
---
+++ temp/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const port = process.env.port || 3003;
+const app = express()
+
+app.listen(port, () => {
+  console.log("Server is up and listening on 3003...")
+})
+
+app.get("/", (req, res) => {
+    console.log("hiya!");
+    res.send("hiya!");
+    res.end()
+})
+
+//ROUTE 1: Simple GET Request 
+app.get('/user/:username', (req, res) => {
+    console.log("Fetching user with id: " + req.params.username);  
+    res.send("User " + req.params.username);
+    res.end();
+})
+
+//ROUTE 2: Simple Users Response
+app.get("/users", (req, res) => {
+    const user1 = {firstName: "David", lastName: "V"}
+    const user2 = {firstName: "Frodo", lastName: "B"}
+    const user3 = {firstName: "Bilbo", lastName: "B"}
+    res.json([user1, user2, user3])
+})
+
+
+ 
