1f4f09a9723fe58a0329fac7f787dc1bcd739616
David <vasquezd@oregonstate.edu>
2021-06-16 17:02:02 -0700

    updates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const PORT = process.env.PORT || 3003;
 const app = express()
+var mysql = require('mysql');
 
 app.listen(PORT, () => {
   console.log("Server is up and listening on " + PORT)
@@ -26,6 +27,52 @@ app.get("/users", (req, res) => {
     const user3 = {firstName: "Bilbo", lastName: "B"}
     const user4 = {firstName: "Sam", lastName: "G"}
     res.json([user1, user2, user3])
+})
+
+
+//ROUTE 2: Simple Users Response
+app.get("/database", (req, res) => {
+
+    /*
+    var connection = mysql.createConnection({
+      host     : process.env.RDS_HOSTNAME,
+      user     : process.env.RDS_USERNAME,
+      password : process.env.RDS_PASSWORD,
+      port     : process.env.RDS_PORT
+    });
+      host: 'shareshare.c3itguipg2wt.us-west-2.rds.amazonaws.com',
+      user: 'admin',
+      password: 'gCtLRbXMWWS2SwNg',
+      database: 'shareshare'       
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'shareshare'
+*/
+
+    var connection = mysql.createConnection({
+        host: 'shareshare.c3itguipg2wt.us-west-2.rds.amazonaws.com',
+        user: 'admin',
+        password: 'gCtLRbXMWWS2SwNg',
+        database: 'shareshare' 
+    });
+
+    const output = ""
+    
+    connection.connect(function(err) {
+      if (err) {
+        output = 'Database connection failed: ' + err.stack;
+        console.error('Database connection failed: ' + err.stack);
+        return;
+      } else {
+        output = 'worked!';
+      }
+    
+      console.log('Connected to database.');
+    });
+    
+    connection.end();
+    res.send({connected: output})
 })
 
 
