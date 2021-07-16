d2024a7fc76ccaa98b5b11d01026e339bf2f38f5
David <vasquezd@oregonstate.edu>
2021-07-16 14:40:42 -0700

    updates

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const mysql = require('mysql');
 const posts = require('./application/routes/posts.js');
 app.use(express.json());
+app.use(posts);
 
 app.listen(PORT, () => {
   console.log("Server is up and listening on " + PORT)
