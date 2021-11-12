dc01e38a9712b8ad345327cfc2884fc3b66d757c
David <vasquezd@oregonstate.edu>
2021-11-11 16:18:15 -0800

    updates

--- application/routes/uploadPhoto.js
+++ application/routes/uploadPhoto.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+//require('dotenv').config()
 const express = require('express')
 const uploadRouter = express.Router();
 const postFunctions = require('../../functions/postFunctions')
