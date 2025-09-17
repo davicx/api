a518218e8a40bcae4aa9da0bbf7e2c8f9959a45b
David <vasquezd@oregonstate.edu>
2025-09-17 15:38:09 -0700

    Create conn.js

---
+++ application/functions/conn.js
@@ -0,0 +1,21 @@
+//const mysql = require('mysql')
+const mysql = require('mysql2');
+
+
+//CONNECTION
+//MYSQL 2/
+const pool = mysql.createPool({
+   connectionLimit: 10,
+   host: 'localhost',
+   user: 'root',
+   password: 'mypassword',
+   database: 'shareshare'
+})
+
+//Functions: Get Connectionno
+function getConnection() {
+   return pool;
+}
+
+
+module.exports = { getConnection };
