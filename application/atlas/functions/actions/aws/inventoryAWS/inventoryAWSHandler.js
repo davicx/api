fca7de751d10ad2eaa1cc9077dcf85fa793e8f8f
David V <davidvas@hawaii.edu>
2026-05-19 17:07:24 -0700

    updates

--- application/atlas/functions/actions/aws/inventoryAWS/inventoryAWSHandler.js
+++ application/atlas/functions/actions/aws/inventoryAWS/inventoryAWSHandler.js
@@ -1,11 +1,60 @@
+const atlasAWSFunctions = require('../atlasAWSFunctions');
+const atlasAWSInventoryFormatter = require('./atlasAWSInventoryFormatter');
+const atlasAWSInventoryMessageBuilder = require('./atlasAWSInventoryMessageBuilder');
+
 async function inventoryAWSHandler() {
 
-    return {
-        success: false,
-        cloudPilotMessage: "AWS inventory execution handler is wired but not implemented yet.",
-        atlasResponse: null,
-        error: "inventory_aws_handler_not_implemented"
-    };
+    try {
+
+        let atlasResponseFormatted = null;
+
+        const atlasResponseRaw =
+            await atlasAWSFunctions.inventoryAWS();
+
+        console.log("_____________________________________");
+        console.log("RAW Atlas AWS Inventory Response:");
+        console.log(JSON.stringify(atlasResponseRaw, null, 2));
+        console.log("_____________________________________");
+
+        if (
+            atlasResponseRaw?.success === true &&
+            atlasResponseRaw?.data
+        ) {
+
+            atlasResponseFormatted =
+                atlasAWSInventoryFormatter.formatAtlasAWSInventoryOutput(
+                    atlasResponseRaw
+                );
+        }
+
+        console.log("_____________________________________");
+        console.log("Atlas AWS Inventory Response:");
+        console.log(atlasResponseFormatted);
+        console.log("_____________________________________");
+
+        return {
+            success: true,
+            cloudPilotMessage:
+                atlasAWSInventoryMessageBuilder.buildAWSInventoryMessage(
+                    atlasResponseFormatted
+                ),
+            error: null,
+            atlasResponse: atlasResponseFormatted
+        };
+
+    } catch (error) {
+
+        console.log("Atlas AWS Inventory Error:");
+        console.log(error);
+
+        return {
+            success: false,
+            cloudPilotMessage:
+                "I could not complete the AWS inventory.",
+            error: error.message,
+            atlasResponse: null
+        };
+    }
 }
 
 module.exports = inventoryAWSHandler;
